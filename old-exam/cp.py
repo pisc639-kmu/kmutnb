a026f1a26d5ad1bd98b37833ea86711129503d28
pisc639-kmu <s6803102118226@email.kmutnb.ac.th>
2026-06-19 21:54:49 +0700

    finishing old-exam/cb/1-2-m

--- old-exam/cp.py
+++ old-exam/cp.py
@@ -10,18 +10,10 @@ import shutil
 with open("subjects.json", "r", encoding="utf-8") as f:
     subjects_json = json.load(f)
 
-subjects = {
-    "394171": "Math1",
-    "031001131": "Math1",
-    "394173": "Math3",
-    "031001133": "Math3",
-    "393141": "English",
-    "031001141": "English",
-    "392131": "Physics",
-    "031001111": "Physics",
-    "393161": "Thai",
-    "031001151": "Thai",
-}
+with open("subjects.min.json", "r", encoding="utf-8") as f:
+    subjects = json.load(f)
+subjects = subjects["cb"]
+subjects = subjects["1-2"]
 
 def merge(inputs, output):
     merger = PdfWriter()
@@ -45,60 +37,47 @@ def main():
     path_regex = re.compile(r'([a-zA-Z]+:\\(?:[^\\/:*?"<>|\r\n]+\\)*.*?(?=\s+[a-zA-Z]+:\\|$))')
     paths = r"""
 
-D:\kmutnb\old-exam\2555\term 1\final\394171.pdf
-D:\kmutnb\old-exam\2556\term 1\final\394171.pdf
-D:\kmutnb\old-exam\2557\term 1\final\394171.pdf
-D:\kmutnb\old-exam\2558\term 1\final\394171.pdf
-D:\kmutnb\old-exam\2560\term 1\final\394171.pdf
-D:\kmutnb\old-exam\2561\term 1\final\27 nov 2561\27 nov 61\394171.pdf
-D:\kmutnb\old-exam\2562\term 1\final\394171.pdf
-D:\kmutnb\old-exam\2563\term 1\final\28 oct 63\394171.pdf
-D:\kmutnb\old-exam\2567\term 1\final\031001131 s1-14.pdf
-D:\kmutnb\old-exam\2568\term 1\final\031001131 s1-14.pdf
+D:\kmutnb\old-exam\2556\term 2\midterm\394172.pdf
+D:\kmutnb\old-exam\2557\term 2\midterm\394172.pdf
+D:\kmutnb\old-exam\2558\term 2\midterm\394172.pdf
+D:\kmutnb\old-exam\2560\term 2\midterm\5 mar 61\394172.pdf
+D:\kmutnb\old-exam\2561\term 2\midterm\394172.pdf
+D:\kmutnb\old-exam\2562\term 2\midterm\21 jan 63\394172.pdf
+D:\kmutnb\old-exam\2567\term 2\midterm\031001132 s1-14.pdf
+D:\kmutnb\old-exam\2568\term 2\midterm\031001132 s1-14.pdf
 
-D:\kmutnb\old-exam\2556\term 1\final\394173.pdf
-D:\kmutnb\old-exam\2557\term 1\final\394173.pdf
-D:\kmutnb\old-exam\2558\term 1\final\394173.pdf
-D:\kmutnb\old-exam\2560\term 1\final\394173.pdf
-D:\kmutnb\old-exam\2561\term 1\final\28 nov 61\394173.pdf
-D:\kmutnb\old-exam\2562\term 1\final\394173.pdf
-D:\kmutnb\old-exam\2563\term 1\final\27 oct 63\394173.pdf
-D:\kmutnb\old-exam\2567\term 1\final\031001133 s1-14.pdf
-D:\kmutnb\old-exam\2568\term 1\final\031001133 s1-14.pdf
+D:\kmutnb\old-exam\2556\term 2\midterm\394174.pdf
+D:\kmutnb\old-exam\2557\term 2\midterm\394174.pdf
+D:\kmutnb\old-exam\2558\term 2\midterm\394174.pdf
+D:\kmutnb\old-exam\2560\term 2\midterm\6 mar 61\394174.pdf
+D:\kmutnb\old-exam\2561\term 2\midterm\394174.pdf
+D:\kmutnb\old-exam\2562\term 2\midterm\21 jan 63\394174.pdf
+D:\kmutnb\old-exam\2567\term 2\midterm\031001134 s1-14.pdf
+D:\kmutnb\old-exam\2568\term 2\midterm\031001134 s1-14.pdf
 
-D:\kmutnb\old-exam\2555\term 1\final\393141.pdf
-D:\kmutnb\old-exam\2556\term 1\final\393141.pdf
-D:\kmutnb\old-exam\2557\term 1\final\393141.pdf
-D:\kmutnb\old-exam\2558\term 1\final\393141.pdf
-D:\kmutnb\old-exam\2560\term 1\final\393141.pdf
-D:\kmutnb\old-exam\2561\term 1\final\30 nov 61\393141.pdf
-D:\kmutnb\old-exam\2562\term 1\final\393141.pdf
-D:\kmutnb\old-exam\2563\term 1\final\26 oct 63\393141.pdf
-D:\kmutnb\old-exam\2567\term 1\final\031001141 s1-14.pdf
-D:\kmutnb\old-exam\2568\term 1\final\031001141 s1-17.pdf
+D:\kmutnb\old-exam\2556\term 2\midterm\393162.pdf
+D:\kmutnb\old-exam\2557\term 2\midterm\393162.pdf
+D:\kmutnb\old-exam\2558\term 2\midterm\393162.pdf
+D:\kmutnb\old-exam\2560\term 2\midterm\5 mar 61\393162.pdf
+D:\kmutnb\old-exam\2561\term 2\midterm\393162.pdf
+D:\kmutnb\old-exam\2562\term 2\midterm\20 jan 63\393162.pdf
+D:\kmutnb\old-exam\2567\term 2\midterm\031001152 s1-14.pdf
+D:\kmutnb\old-exam\2568\term 2\midterm\031001152 s1-14.pdf
 
-D:\kmutnb\old-exam\2555\term 1\final\392131.pdf
-D:\kmutnb\old-exam\2556\term 1\final\392131.pdf
-D:\kmutnb\old-exam\2557\term 1\final\392131.pdf
-D:\kmutnb\old-exam\2558\term 1\final\392131.pdf
-D:\kmutnb\old-exam\2560\term 1\final\392131.pdf
-D:\kmutnb\old-exam\2561\term 1\final\28 nov 61\392131.pdf
-D:\kmutnb\old-exam\2562\term 1\final\392131.pdf
-D:\kmutnb\old-exam\2563\term 1\final\29 oct 63\392131.pdf
-D:\kmutnb\old-exam\2567\term 1\final\031001111 s1-14.pdf
-D:\kmutnb\old-exam\2568\term 1\final\031001111 s1-14.pdf
+D:\kmutnb\old-exam\2567\term 2\midterm\031001142 s1-14.pdf
+D:\kmutnb\old-exam\2568\term 2\midterm\031001142 s1-17.pdf
 
-D:\kmutnb\old-exam\2556\term 1\final\393161.pdf
-D:\kmutnb\old-exam\2557\term 1\final\393161.pdf
-D:\kmutnb\old-exam\2558\term 1\final\393161.pdf
-D:\kmutnb\old-exam\2560\term 1\final\393161.pdf
-D:\kmutnb\old-exam\2561\term 1\final\26 nov 2561\393161.pdf
-D:\kmutnb\old-exam\2563\term 1\final\30 oct 63\393161.pdf
-D:\kmutnb\old-exam\2567\term 1\final\031001151 s1-18.pdf
-D:\kmutnb\old-exam\2568\term 1\final\031001151 s1-14.pdf
+D:\kmutnb\old-exam\2556\term 2\midterm\395181.pdf
+D:\kmutnb\old-exam\2557\term 2\midterm\395181.pdf
+D:\kmutnb\old-exam\2558\term 2\midterm\395181.pdf
+D:\kmutnb\old-exam\2560\term 2\midterm\6 mar 61\395181.pdf
+D:\kmutnb\old-exam\2561\term 2\midterm\395181.pdf
+D:\kmutnb\old-exam\2562\term 2\midterm\21 jan 63\395181.pdf
+D:\kmutnb\old-exam\2567\term 2\midterm\031001161 s1-14.pdf
+D:\kmutnb\old-exam\2568\term 2\midterm\031001161 s1-14.pdf
 
 """.strip().split("\n")
-    dest = Path(os.path.expandvars("%onedrive%")) / "KMU" / "old-exam" / "cb" / "1-1-f"
+    dest = Path(os.path.expandvars("%onedrive%")) / "KMU" / "old-exam" / "cb" / "1-2-m"
     # subject = "Math1"
     # path = map(Path, path)
     for path in paths:
