44afd9c2f9df0f0fc6f8a4c8bb857b6cba69c4b7
pisc639-kmu <s6803102118226@email.kmutnb.ac.th>
2026-07-28 12:50:44 +0700

    add script.min.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+(()=>{
+const _w = window;
+const _document = document;
+const _document_body = _document.body
+const _querySelector = (element, selector) => element.querySelector(selector);
+const _querySelectorAll = (element, selector) => element.querySelectorAll(selector);
+const _getElementById = (element, id) => element.getElementById(id);
+const _document_querySelector = (selector) => _querySelector(_document, selector);
+const _document_querySelectorAll = (selector) => _querySelectorAll(_document, selector);
+const _document_getElementById = (id) => _getElementById(_document, id);
+const _document_createElement = (element) => _document.createElement(element);
+const _classList = (element) => element.classList;
+const _classList_add = (element, ...className) => _classList(element).add(...className);
+const _classList_remove = (element, ...className) => _classList(element).remove(...className);
+const _classList_toggle = (element, ...className) => _classList(element).toggle(...className);
+const _setAttribute = (element, attribute, value) => element.setAttribute(attribute, value);
+const _innerHTML = (element, html) => element.innerHTML = html;
+const _appendChild = (element, child) => element.appendChild(child);
+const _addEventListener = (element, event, callback) => element.addEventListener(event, callback);
+
     // function insertNav() {
     //     nav = `
     // <span>
@@ -29,51 +49,51 @@
     //     const doc = parser.parseFromString(nav, "text/html");
     //     const elem = doc.querySelector("span");
 
-    //     document.body.prepend(elem);
+    //     d.body.prepend(elem);
     // };
 
     // // insertNav();
 
-const mobileMenuButton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-const menuIconOpen = document.getElementById('menu-icon-open');
-const menuIconClose = document.getElementById('menu-icon-close');
+const mobileMenuButton = _getElementById('mobile-menu-button');
+const mobileMenu = _getElementById('mobile-menu');
+const menuIconOpen = _getElementById('menu-icon-open');
+const menuIconClose = _getElementById('menu-icon-close');
 
-mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-    const isHidden = mobileMenu.classList.contains('hidden');
+_addEventListener(mobileMenuButton, 'click', () => {
+    _classList_toggle(mobileMenu, 'hidden');
+    const isHidden = _classList(mobileMenu).contains('hidden');
 
     if (isHidden) {
         // Menu is closed, show hamburger icon
-        menuIconOpen.classList.remove('opacity-0');
-        menuIconClose.classList.add('opacity-0');
+        _classList_remove(menuIconOpen, 'opacity-0');
+        _classList_add(menuIconClose, 'opacity-0');
         
     } else {
         // Menu is open, show close icon
-        menuIconOpen.classList.add('opacity-0');
-        menuIconClose.classList.remove('opacity-0');
+        _classList_add(menuIconOpen, 'opacity-0');
+        _classList_remove(menuIconClose, 'opacity-0');
         
-        // window.scrollTo(0, 1000);
-        window.scrollTo({ top: 0, behavior: "smooth" });
+        // _w.scrollTo(0, 1000);
+        _w.scrollTo({ top: 0, behavior: "smooth" });
     }
 });
 
 // Close the mobile menu when a link is clicked
 const mobileLinks = mobileMenu.querySelectorAll('a');
 mobileLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        mobileMenu.classList.add('hidden');
-        menuIconOpen.classList.remove('opacity-0');
-        menuIconClose.classList.add('opacity-0');
+    _addEventListener(link, 'click', () => {
+        _classList_add(mobileMenu, 'hidden');
+        _classList_remove(menuIconOpen, 'opacity-0');
+        _classList_add(menuIconClose, 'opacity-0');
     });
 });
 
-// Close the mobile menu on window resize if screen becomes desktop size
-window.addEventListener('resize', () => {
-    if (window.innerWidth >= 768) {
-        mobileMenu.classList.add('hidden');
-        menuIconOpen.classList.remove('opacity-0');
-        menuIconClose.classList.add('opacity-0');
+// Close the mobile menu on _w resize if screen becomes desktop size
+_addEventListener(_w, 'resize', () => {
+    if (_w.innerWidth >= 768) {
+        _classList_add(mobileMenu, 'hidden');
+        _classList_remove(menuIconOpen, 'opacity-0');
+        _classList_add(menuIconClose, 'opacity-0');
     }
 });
 
@@ -90,47 +110,57 @@ window.addEventListener('resize', () => {
 //     expandedContent.classList.toggle('hidden');
 // }
 
-function toggleContent(element) {
-    element.querySelector('.expand-button').classList.toggle('rotate-180');
-    const topContent = element.querySelector('.top-content');
-    topContent.classList.toggle('rounded-t-md');
-    topContent.classList.toggle('rounded-md');
-    topContent.classList.toggle('hover:bg-neutral-200');
-    topContent.classList.toggle('hover:dark:bg-neutral-700');
-    topContent.classList.toggle('bg-neutral-200');
-    topContent.classList.toggle('dark:bg-neutral-700');
-    const expandedContent = element.parentElement.querySelector('.content-expanded');
-    expandedContent.classList.toggle('hidden');
-}
+// function toggleContent(element) {
+//     _classList_toggle(_document_querySelector(element, '.expand-button'), 'rotate-180');
+//     const topContent = element.querySelector('.top-content');
+//     // topContent.classList.toggle('rounded-t-md');
+//     // topContent.classList.toggle('rounded-md');
+//     // topContent.classList.toggle('hover:bg-neutral-200');
+//     // topContent.classList.toggle('hover:dark:bg-neutral-700');
+//     // topContent.classList.toggle('bg-neutral-200');
+//     // topContent.classList.toggle('dark:bg-neutral-700');
+//     ['rounded-t-md', 'rounded-md', 'hover:bg-neutral-200', 'hover:dark:bg-neutral-700', 'bg-neutral-200', 'dark:bg-neutral-700'].forEach(a => _classList_toggle(topContent, a));
+//     const expandedContent = _querySelector(element.parentElement, '.content-expanded');
+//     _classList_toggle(expandedContent, 'hidden');
+// }
 
-function clockFormDiv() {
-    const div = document.createElement('div');
-    const form = document.createElement('form');
-    form.id = 'clock-form';
-    form.action = '/clock';
-    form.method = 'get';
-    div.appendChild(form);
+// function clockFormDiv() {
+//     const div = _document_createElement('div');
+//     const form = _document_createElement('form');
+//     form.id = 'clock-form';
+//     form.action = '/clock';
+//     form.method = 'get';
+//     _innerHTML(div,form);
 
-    const button = document.createElement('button');
-    button.id = 'clock-page-button';
-    button.classList.add('bg-white', 'dark:bg-neutral-800', 'hover:bg-neutral-200', 'dark:hover:bg-neutral-700', 'border', 'border-neutral-200', 'dark:border-neutral-700', 'shadow-md', 'w-12', 'h-12', 'rounded-lg', 'flex', 'items-center', 'justify-center', 'p-2');
+//     const button = _document_createElement('button');
+//     button.id = 'clock-page-button';
+//     _classList_add(button, 'bg-white', 'dark:bg-neutral-800', 'hover:bg-neutral-200', 'dark:hover:bg-neutral-700', 'border', 'border-neutral-200', 'dark:border-neutral-700', 'shadow-md', 'w-12', 'h-12', 'rounded-lg', 'flex', 'items-center', 'justify-center', 'p-2');
 
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    svg.setAttribute("viewBox", "0 0 24 24");
-    svg.setAttribute("fill", "none");
-    svg.setAttribute("stroke", "currentColor");
-    svg.setAttribute("stroke-width", "2");
-    svg.setAttribute("stroke-linecap", "round");
-    svg.setAttribute("stroke-linejoin", "round");
-    svg.classList.add('w-6', 'h-6');
-    svg.innerHTML = '<path d="M19 9l-7 7-7-7"/>';
+//     const svg = _document_createElement("http://www.w3.org/2000/svg", "svg");
+//     // svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+//     // svg.setAttribute("viewBox", "0 0 24 24");
+//     // svg.setAttribute("fill", "none");
+//     // svg.setAttribute("stroke", "currentColor");
+//     // svg.setAttribute("stroke-width", "2");
+//     // svg.setAttribute("stroke-linecap", "round");
+//     // svg.setAttribute("stroke-linejoin", "round");
+//     _setAttribute(svg, "xmlns", "http://www.w3.org/2000/svg");
+//     _setAttribute(svg, "viewBox", "0 0 24 24");
+//     _setAttribute(svg, "fill", "none");
+//     _setAttribute(svg, "stroke", "currentColor");
+//     _setAttribute(svg, "stroke-width", "2");
+//     _setAttribute(svg, "stroke-linecap", "round");
+//     _setAttribute(svg, "stroke-linejoin", "round");
+//     // svg.classList.add('w-6', 'h-6');
+//     _classList_add(svg, 'w-6', 'h-6');
+//     // svg.innerHTML = '<path d="M19 9l-7 7-7-7"/>';
+//     _innerHTML(svg, '<path d="M19 9l-7 7-7-7"/>');
 
-    button.appendChild(svg);
-    form.appendChild(button);
+//     _appendChild(button, svg);
+//     _appendChild(form, button);
     
-    return div;
-}
+//     return div;
+// }
 
 function createClockForm() {
     // const clockFormHtml = `
@@ -151,16 +181,21 @@ function createClockForm() {
     //     </form>
     // </div>
     // `;
-    const clockFormHtml = clockFormDiv();
+
+    
+    const clockFormHtml = '<div><form id="clock-form" action="../clock" emthod="get"><button id="clock-page-button" class="bg-white dark:bg-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-700 border border-neutral-200 dark:border-neutral-700 shadow-md w-12 h-12 rounded-lg flex items-center justify-center p-2 bottom-4 right-4 fixed"><svg class="transform transition-transform duration-300 justify-center" fill="none" stroke="currentColor" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg"><path id="" class="" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M 6 1 A 1 1 0 0 0 6 11 A 1 1 0 0 0 6 1 Z M 6 3 L 6 6 L 8 7"></path></svg></button></form></div>';
+
+    // const clockFormHtml = clockFormDiv();
 
     const parser = new DOMParser();
     const doc = parser.parseFromString(clockFormHtml, 'text/html');
-    const clockForm = doc.getElementById('clock-form');
-    document.body.appendChild(clockForm);
+    const clockForm = _getElementById(doc, 'clock-form');
+    _appendChild(_document_body, clockForm);
 }
 
 createClockForm();
 
-document.body.setAttribute('class', '');
+_setAttribute(_document_body, 'class', '');
 // $('body').removeClass();
-document.body.classList.add("dark", "bg-gray-50", "dark:bg-gray-950", "min-h-screen")
+_classList_add(_document_body, "dark", "bg-gray-50", "dark:bg-gray-950", "min-h-screen")
+})();
